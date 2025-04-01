5eed1b0adb623c618a791c826b5a7522755f2623
schmidt-ohm <s.schmidt@ohm-gymnasium.de>
2025-04-01 13:00:51 +0200

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,31 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+
+/**
+ * 
+ */
+public class Person
+{
+    private int alter;
+    private String name;
+    private String adr;
+
+    /**
+     * 
+     */
+    public Person(String name_neu, int alter_neu)
+    {
+        name = name_neu;
+        adr = "Erlangen";
+        alter = alter_neu;
+    }
+
+    /**
+     * 
+     */
+    public void ausgeben()
+    {
+        System.out.println(adr);
+        System.out.println(name);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+
+/**
+ * 
+ */
+public class Test
+{
+    private int x = 0;
+    private String name = "Hallo";
+
+    /**
+     * Constructor for objects of class Test
+     */
+    public Test(String name)
+    {
+        name = "Hallo du";
+    }
+}
